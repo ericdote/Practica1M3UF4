554bb89a1c8020ce0c5329e96b9c9070c779ab99
ericdote <pokemon13>
2016-10-19 21:13:57 +0200

    CORRECION JOAN, FUNCIONA!!!! v3

--- src/practica1/Joc.java
+++ src/practica1/Joc.java
@@ -43,54 +43,37 @@ public class Joc {
     
     private void crearJugadors() {
         nomJugador = new ArrayList();
-        //For per asignar els noms als jugadors
+        //For per asignar els noms als nomJugador
         for (int i = 0; i < 4; i++) {
             nomJugador.add(new Jugador("Jugador" + (i + 1)));
         }
     }
     
-    private void asignarFichas() {
-        //Metode per asignar les fiches
-        while (fichas.size() != 0) {
-            for (int j = 0; j < 4; j++) {
-                Random fichaAleatoria = new Random();
-                int aleatorio = fichaAleatoria.nextInt(fichas.size());
-                nomJugador.get(j).getFichasJugador().add(fichas.get(aleatorio));
-//                nomJugador.get(j).añadirFicha(fichas.get(aleatorio));
-                fichas.remove(aleatorio);
+      private void asignarFichas() {
+        boolean[] utilitzades = new boolean[fichas.size()];
+        int contador = 0;
+        while (contador != fichas.size()) {
+            for (int i = 0; i < nomJugador.size(); i++) {
+                Random r = new Random();
+                int ale = r.nextInt(fichas.size());
+                while (utilitzades[(ale = r.nextInt(fichas.size()))]);
+                nomJugador.get(i).getFichasJugador().add(fichas.get(ale));
+                utilitzades[ale] = true;
+                contador++;
             }
         }
     }
-    
-    private int jugadorInicial() {
-        //Recorre los diferentes jugadores mirando las fichas que tiene en conjunto con el metodo que tenemos en Jugador para ir mirando las fichas en el ArrayList
-        //Del Jugador que le toca, si es el jugador lo muestra por pantalla y lo asigna a una variable para comenzar con el la partida.
-//        int contador=0; 
-        int jugador = 0;
-//        Ficha f = ;
-        for (Jugador j : nomJugador) {
-            if (j.getFichasJugador().contains(new Ficha(6, 6))){
-                System.out.println(j.getNom());
-                jugador = nomJugador.indexOf(j);
-            }
-//            System.out.println(nomJugador.indexOf(j));
-//            if (j.getFichasJugador().contains(f)) {
-//                jugador = nomJugador.indexOf(j);
-////                jugador = contador;
-//                break;
-//            }
-//            contador++;
-        }
 
-//        ArrayList<Ficha> fichas = jug.getFichasJugador();
-//        for (int i = 0; i < nomJugador.size(); i++) {
-//            if (nomJugador.get(i).trobarFichaIncial()) {
-//                jugador = i;
-//                break;
-//            }
-//        }
+    //  Restem -48 per que es on començen els numerics a la taula ASCII.
+    private int jugadorInicial() {
+        int jugador = 0;
+        for (Jugador j : nomJugador) {
+            if (j.getFichasJugador().contains(fichas.get(fichas.size() - 1))) {
+                jugador = nomJugador.indexOf(j);
+                break;
+            }
+        }
         return jugador;
-        
     }
     
 //    public void turnos() {
