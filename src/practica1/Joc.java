459f927f6fc462ddf834d73a4c04324533331e12
Eric <Eric@192.168.1.39>
2016-10-20 00:55:59 +0200

    Agregadas clases (algunas por completar)en clase Juego

--- src/practica1/Joc.java
+++ src/practica1/Joc.java
@@ -6,41 +6,41 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Joc {
-    
+
     Scanner lector = new Scanner(System.in);
     private ArrayList<Ficha> fichas;
     private ArrayList<Jugador> nomJugador;
-//    private int jugador;
     private int turno;
     private ArrayDeque<Ficha> fichasTablero;
-    
+    public int jugadorTorn;
+    private Jugador jug;
+    private String nom;
+    private int pasar;
+    private String guanyador;
+
     public Joc() {
         fichasTablero = new ArrayDeque();
         afegirFichas();
         crearJugadors();
-        
         asignarFichas();
-        for(Jugador j : nomJugador){
-            System.out.println(j.toString());
-        }
-        System.out.println(jugadorInicial());
-
-//        turnos();
+        jugadorTorn = 0;
+        jugadorInicial();
+        actualitzarJoc();
     }
-    
+
     private void afegirFichas() {
         fichas = new ArrayList();
-        
+
         //Doble bucle de for per afegir les 28 fiches
         for (int i = 0; i <= 6; i++) {
             for (int j = i; j <= 6; j++) {
                 fichas.add(new Ficha(i, j));
             }
-            
+
         }
-        
+
     }
-    
+
     private void crearJugadors() {
         nomJugador = new ArrayList();
         //For per asignar els noms als nomJugador
@@ -48,8 +48,8 @@ public class Joc {
             nomJugador.add(new Jugador("Jugador" + (i + 1)));
         }
     }
-    
-      private void asignarFichas() {
+
+    private void asignarFichas() {
         boolean[] utilitzades = new boolean[fichas.size()];
         int contador = 0;
         while (contador != fichas.size()) {
@@ -64,20 +64,49 @@ public class Joc {
         }
     }
 
-    //  Restem -48 per que es on començen els numerics a la taula ASCII.
     private int jugadorInicial() {
-        int jugador = 0;
         for (Jugador j : nomJugador) {
             if (j.getFichasJugador().contains(fichas.get(fichas.size() - 1))) {
-                jugador = nomJugador.indexOf(j);
+                jugadorTorn = nomJugador.indexOf(j);
+                nom = nomJugador.toString();
                 break;
             }
         }
-        return jugador;
+        System.out.println(jugadorTorn);
+        System.out.println(nom);
+        return jugadorTorn;
     }
-    
-//    public void turnos() {
-//        turno = jugador;
-//        
-//    }
-}
+
+    public void actualitzarJoc() {
+        jug = new Jugador(nom);
+        if (jug.getFichasJugador().equals(0)) {
+            guanyador = jug.getNom(); //Esto habria que comprobarlo, ya que en principio gana el jugador que se queda sin fichas
+                                      //Per falta por saber si es correcto tener guardado al ganador por su nombre en vez de id int.
+            finalitzaJoc();
+        } else if (pasar == 4) {
+            finalitzaJoc();
+        } else {
+            jugadorTorn = (jugadorTorn + 1) % 4;
+        }
+    }
+
+    public int calculaGuanyador() {
+
+        //No se si usar Array o ArrayList para calcular los puntos y compararlos
+        int valor = 0;
+        if (pasar == 4) {
+            for (Jugador j : nomJugador) {
+                for (Ficha f : j.getFichasJugador()) {
+                    //Completar sumar valor fichas para calcular ganador en caso de todos pasar
+                }
+            }
+        } 
+
+        return 0;
+    }
+
+    public void finalitzaJoc() {
+            System.exit(0);
+        }
+    }
+
--- src/practica1/Jugada.java
+++ src/practica1/Jugada.java
@@ -1,40 +1,40 @@
-package practica1;
-
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-
-public class Jugada {
-
-    private int turno;
-
-    private boolean inicial = false;
-//    private Jugador jug = new Jugador();
-    private int posicion;
-
-    public Jugada(int turno) {
-        this.turno = turno;
-
-    }
-
-    public Jugada() {
-
-    }
-
-
-    public void añadirFicha(Ficha ficha, int posicion) {
-        Jugador jug = new Jugador();
-        if (inicial == false) {
-            for (Ficha j : jug.getFichasJugador()) {
-                if (j.getDreta() == 6 && j.getEsquerra() == 6) {
-                    fichasTablero.add(j);
-                    jug.getFichasJugador().remove(j);
-                    inicial = true;
-                    break;
-                }
-            }
-        } else if(posicion == 1){
-            
-        }
-    }
-
-}
+//package practica1;
+//
+//import java.util.ArrayDeque;
+//import java.util.ArrayList;
+//
+//public class Jugada {
+//
+//    private int turno;
+//
+//    private boolean inicial = false;
+////    private Jugador jug = new Jugador();
+//    private int posicion;
+//
+//    public Jugada(int turno) {
+//        this.turno = turno;
+//
+//    }
+//
+//    public Jugada() {
+//
+//    }
+//
+//
+//    public void añadirFicha(Ficha ficha, int posicion) {
+//        Jugador jug = new Jugador();
+//        if (inicial == false) {
+//            for (Ficha j : jug.getFichasJugador()) {
+//                if (j.getDreta() == 6 && j.getEsquerra() == 6) {
+//                    fichasTablero.add(j);
+//                    jug.getFichasJugador().remove(j);
+//                    inicial = true;
+//                    break;
+//                }
+//            }
+//        } else if(posicion == 1){
+//            
+//        }
+//    }
+//
+//}
